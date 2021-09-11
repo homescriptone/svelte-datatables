c1aff768857d71b6b42b59145548d42758b84634
Manu The Blacker <25031292+manutheblacker@users.noreply.github.com>
2021-09-11 10:20:22 +0100

    Initial commit
    This is a first commit explaining the requirements.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # svelte-datatable-bootstrap-5
 A simple table using Datatable and Bootstrap 5
+
+You'll need to run yarn/npm install Bootstrap 5.
+
+You'll need to import the svelte-datable.css and svelte-datable.js into the Table.svelte component file.
+
+Once done, you can import the component Table and then pass your data so it will display your table.
+
+Marks:
+The data must be like this :
+
+<code>
+    let tableData = {
+        'rows' : [
+
+        ],
+        'columns' : [
+            
+        ]
+    }
+</code>
---
+++ Table.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let tableData = []  ;
+    //import the svelte-datatable.css
+    //import the svelte-datatable.js
+    import 'bootstrap5'
+    import {onMount} from 'svelte'
+    let all_rows, all_columns;
+    onMount(()=>{
+      var dataTableEl = document.getElementById('datatable');
+      const dataTable = new simpleDatatables.DataTable(dataTableEl);
+    });
+  </script>
+  
+  <main>
+      <div class="table-responsive py-4">
+          <table class="table table-flush" id="datatable">
+              <thead class="thead-light">
+              <tr>
+                  {#each tableData.all_columns  as table_col_name }
+                      <th>{table_col_name}</th>
+                  {/each}
+              </tr>
+              </thead>
+              <tbody>
+              {#each tableData.all_rows as rows }
+                  <tr>
+                         <td>Your rows data</td>
+                  </tr>
+              {/each}
+              </tbody>
+          </table>
+      </div>
+  </main>
+  
