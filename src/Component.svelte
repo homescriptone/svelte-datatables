c4662d4111c7eaa207825b6d5922b9550cd1f1fc
Manu The Blacker <25031292+manutheblacker@users.noreply.github.com>
2021-09-12 07:06:28 +0100

    Fix table component rendering issues
    When the table component is called more than once, only one table is showed.
    
    To address this, the table ID is dynamic, and some controls are added before using the thead and tbody styles args from table_data array.

--- src/Component.svelte
+++ src/Component.svelte
@@ -69,27 +69,33 @@
         ],
         'styles' : [],
     };
+
+    const generate_id = (() =>{
+        return '_' + Math.random().toString(36).substr(2, 9);
+    });
+
     import './assets/datatable.css'
     import simpleDatatables from './assets/datatable.js'
 
     import {onMount} from 'svelte'
+    let id = "datatable"+generate_id();
     onMount(()=>{
-      var dataTableEl = document.getElementById('datatable');
+      var dataTableEl = document.getElementById(id);
       new simpleDatatables.DataTable(dataTableEl);
     });
   </script>
   
   <main>
       <div class="table-responsive py-4">
-          <table class="table table-flush" id="datatable">
-              <thead class="svelte-datatable-thead thead-light {table_data.styles.thead}">
+          <table class="table table-flush" id="{id}">
+              <thead class="svelte-datatable-thead thead-light { table_data.styles ? table_data.styles.thead : ""}">
               <tr>
                   {#each table_data.columns  as table_col_name }
                     <th>{table_col_name}</th>
                   {/each}
               </tr>
               </thead>
-              <tbody  class="svelte-datatable-tbody {table_data.styles.tbody}">
+              <tbody  class="svelte-datatable-tbody { table_data.styles ? table_data.styles.tbody : "" }">
                 {#each table_data.rows as table_rows }
                 <tr>
                     {#each table_rows as rows }
